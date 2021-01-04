bc79d6d66ec81cd9cb49d6106792f43dce322f47
polemikal <68484486+polemikal@users.noreply.github.com>
2021-01-04 23:55:13 +0300

    1

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # discord-welcome-bot
+
+A great intro sound for Discord servers!
+
+# Setup the Project
+
+1. Download or clone this project.
+2. Setup the project options from `config.json` file.
+3. Upload the required audio files in `default_files` path and enter audio files name in `config.json`.
+4. Run `app.js` file from terminal. (Example run command: `node app.js`)
+
+**(!) DO NOT FORGET** You must install **FFMPEG** on your system to run.
+
+#📞 Contact
+
+(!) If there is something that can be added to this project or if you have found anything missing in this project, you can let us know and help us improve the project! [Let us know!](https://github.com/polemikal/discord-slash-commands-bot/issues)
+
+**Discord:** polemikal#1221
---
+++ app.js
@@ -0,0 +1,125 @@
+const { Client, VoiceChannel, GuildMember } = require("discord.js");
+
+const fs = require("fs");
+const CONFIG = JSON.parse(fs.readFileSync("./config.json", { encoding: "utf-8" }));
+
+const moment = require("moment");
+require("moment-duration-format");
+
+const Voice = new Client({ fetchAllMembers: true, disableMentions: "none", ws: { intents: ["GUILDS", "GUILD_VOICE_STATES", "GUILD_MESSAGES"] }});
+Voice.voiceConnection = null;
+Voice.channelID = null;
+
+Voice.on("ready", async() => {
+
+    Voice.user.setPresence({
+        status: "dnd",
+        activity: {
+            name: CONFIG.DEFAULTS.ACTIVITY_TEXT
+        }
+    });
+
+    Voice.log(`Voice client \'${Voice.user.username}\' has been activated!`);
+
+    const Guild = Voice.guilds.cache.get(CONFIG.DEFAULTS.GUILD_ID) || Voice.guilds.cache.first();
+    if(!Guild) {
+        Voice.error("Guild not found!");
+        return Voice.destroy();
+    }
+    
+    const Channel = Guild.channels.cache.get(CONFIG.DEFAULTS.VOICE_CHANNEL);
+    if(!Channel) {
+        Voice.error("Channel not found!");
+        return Voice.destroy();
+    }
+
+    Channel.join().then(connection =>{
+            
+        Voice.voiceConnection = connection;
+        Voice.channelID = Channels[index].id;
+        Voice.log("The audio file is playing now...")
+        if(!Channels[index].hasStaff()) playVoice(Voice);
+        else Voice.staffJoined = true;
+
+    }).catch(err => {
+        Voice.error(`Cannot connect to voice channel (${Channel.name}) (${Channel.id}): ` + err.message)
+        return Voice.destroy();
+    });
+
+});
+
+Voice.on("voiceStateUpdate", async(oldState, newState) => {
+    if(
+        newState.channelID && (oldState.channelID !== newState.channelID) &&
+        newState.member.isStaff() &&
+        newState.channelID === Voice.channelID &&
+        !newState.channel.hasStaff(newState.member)
+    ) {
+        Voice.staffJoined = true;
+        return playVoice(Voice);
+    }
+    if( 
+        oldState.channelID && 
+        (oldState.channelID !== newState.channelID) && 
+        newState.member.isStaff() && 
+        oldState.channelID === Voice.channelID &&
+        !oldState.channel.hasStaff()
+    ) {
+        Voice.staffJoined = false;
+        return playVoice(Voice);
+    }
+});
+
+/**
+ * 
+ * @param {Client} Voice 
+ */
+function playVoice(Voice) {
+    if(stop === true) return;
+    try {
+
+        const Path = Voice.staffJoined === true ? "./" + CONFIG.FILES.STAFF : "./" + CONFIG.FILES.WELCOME;
+        Voice.playingVoice = true;
+        Voice.voiceConnection.play(Path, {
+            volume: 1
+        }).on("finish", async() => {
+            Voice.playingVoice = false;
+            if(Voice.staffJoined === true) return;
+            playVoice(Voice);
+        });
+
+    } catch(err) {
+
+        return Voice.log("An occured error while playing voice file: " + err.message);
+        
+    }
+};
+
+Client.prototype.log = function(content) {
+    return console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] [VOICE BOT] ${content}`);
+};
+
+Client.prototype.error = function(content) {
+    return console.error(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] [VOICE BOT] ERR! ${content}`);
+};
+
+VoiceChannel.prototype.hasStaff = function(checkMember = false) {
+    if(this.members.some(m => (checkMember !== false ? m.user.id !== checkMember.id : true) && !m.user.bot && m.roles.highest.position >= m.guild.roles.cache.get(CONFIG.DEFAULTS.MIN_STAFF_ROLE).position)) return true; // m.roles.highest.position >= this.guild.roles.cache.get(CONFIG.DEFAULTS.MIN_STAFF_ROLE).position
+    return false;
+}
+
+VoiceChannel.prototype.getStaffs = function(checkMember = false) {
+    return this.members.filter(m => (checkMember !== false ? m.user.id !== checkMember.id : true) && !m.user.bot && m.roles.highest.position >= m.guild.roles.cache.get(CONFIG.DEFAULTS.MIN_STAFF_ROLE).position).size
+}
+
+GuildMember.prototype.isStaff = function() {
+    if(
+        !this.user.bot &&
+        ([...CONFIG.DEFAULTS.AUTHORS].includes(this.id) ||
+        this.hasPermission("ADMINISTRATOR") ||
+        this.roles.highest.position >= this.guild.roles.cache.get(CONFIG.DEFAULTS.MIN_STAFF_ROLE).position
+        )
+    ) return true;
+    return false;
+}
+
